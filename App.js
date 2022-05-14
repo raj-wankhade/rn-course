37af0e8f6ae454fbd130a3fa91b2cef05ea34074
raj-wankhade <rajwankhade789@gmail.com>
2022-05-14 17:05:55 +0530

    master - basic app with styles

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.textStyle}>Hello World!!!!</Text>
+      </View>
+      <Text style={styles.textStyle}>Another Text!!!!</Text>
+      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+      <Button title="Submit" />
     </View>
   );
 }
@@ -13,8 +16,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    margin: 16,
+    borderColor: "blue",
+    borderWidth: 2,
+    padding: 16,
+  },
+  bigBlue: {
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  red: {
+    color: "red",
   },
 });
